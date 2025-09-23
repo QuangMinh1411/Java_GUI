4ff60bc2feadb2b755a34006b62d540cebb5c124
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2025-09-23 20:51:18 +0700

    CreateImage,Save Image,Node snapshot

---
+++ Learning JavaFX17/JavaFXBook/src/com/jdojo/image/ImageUtil.java
@@ -0,0 +1,60 @@
+package com.jdojo.image;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageUtil {
+    public static void saveToFile(Image image) {
+        // Ask the user for the file name
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select an image file name");
+        fileChooser.setInitialFileName("untitled");
+        FileChooser.ExtensionFilter pngExt = new FileChooser.ExtensionFilter("PNG Files", "*.png");
+        FileChooser.ExtensionFilter jpgExt = new FileChooser.ExtensionFilter("JPEG Files", "*.jpg", "*.jpeg");
+        fileChooser.getExtensionFilters().addAll(pngExt, jpgExt);
+
+        File outputFile = fileChooser.showSaveDialog(null);
+        if (outputFile == null) {
+            return;
+        }
+
+        FileChooser.ExtensionFilter selectedExt = fileChooser.getSelectedExtensionFilter();
+        String imageFormat = "png";
+        if (selectedExt == jpgExt) {
+            imageFormat = "jpg";
+        }
+
+        // Check for the file extension. Add oen, iff not specified
+        String fileName = outputFile.getName().toLowerCase();
+        switch (imageFormat) {
+            case "jpg":
+                if (!fileName.endsWith(".jpeg") && !fileName.endsWith(".jpg")) {
+                    outputFile = new File(outputFile.getParentFile(),
+                            outputFile.getName() + ".jpg");
+                }
+                break;
+            case "png":
+                if (!fileName.endsWith(".png")) {
+                    outputFile = new File(outputFile.getParentFile(),
+                            outputFile.getName() + ".png");
+                }
+        }
+
+        // Convert the image to a buffered image
+        BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+
+        // Save the image to the file
+        try {
+            ImageIO.write(bImage, imageFormat, outputFile);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
